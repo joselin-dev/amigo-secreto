b2c89d6c4ed7faac5d8cf27996dc45b71e1716c9
Joselin <joselinstefania10@gmail.com>
2025-09-10 22:25:38 -0500

    agraga array

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let amigo = inputAmigo.value;
+}
