6388c0a77236c8b3d4c20e2c270685b7af09c3c6
Joselin <joselinstefania10@gmail.com>
2025-09-14 01:02:07 -0500

    agrego funciones

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let cantidad = amigos.length
+
 
 function agregarAmigo() {
     let inputAmigo = document.getElementById("amigo");
     let amigo = inputAmigo.value;
+
+    amigos.push(amigo);
+    limpiarCaja();
+    let mostrarNombre = document.querySelector("#listaAmigos");
+    mostrarNombre.innerHTML = amigos;
+    
+}
+
+
+function limpiarCaja() {
+       document.querySelector("#amigo").value = "";
+}
+
+
+function sortearAmigos() {
+       let sorteo = Math.floor(Math.random()*cantidad)-1;
+       let nombreSorteado = amigos[sorteo];
+
 }
