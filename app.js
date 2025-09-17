943c1b17a6d7f54e42c5d00af55820478062d015
Joselin <joselinstefania10@gmail.com>
2025-09-17 01:46:29 -0500

    actualiza formulas

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
-let cantidad = amigos.length
+
 
 
 function agregarAmigo() {
     let inputAmigo = document.getElementById("amigo");
     let amigo = inputAmigo.value;
 
-    amigos.push(amigo);
-    limpiarCaja();
-    let mostrarNombre = document.querySelector("#listaAmigos");
-    mostrarNombre.innerHTML = amigos;
+    if (amigo != "") {
+       amigos.push(amigo);
+       limpiarCaja();
+       let mostrarNombre = document.querySelector("#listaAmigos");
+       mostrarNombre.innerHTML = amigos.join("<br>");
+       
+    } else{
+       alert("Por favor, ingrese un nombre valido");
+
+       }
     
 }
-
 
 function limpiarCaja() {
        document.querySelector("#amigo").value = "";
 }
 
+function sortearAmigo() {
+       let cantidad = amigos.length;
+       let sorteo = Math.floor(Math.random()*cantidad)+1;
+       let nombreSorteado = amigos[sorteo-1];
+       console.log(nombreSorteado);
+       
+       let mostrarResultado = document.querySelector("#resultado");
+       mostrarResultado.innerHTML = `El amigo secreto sorteado es: ${nombreSorteado}`;
 
-function sortearAmigos() {
-       let sorteo = Math.floor(Math.random()*cantidad)-1;
-       let nombreSorteado = amigos[sorteo];
+
 
 }
